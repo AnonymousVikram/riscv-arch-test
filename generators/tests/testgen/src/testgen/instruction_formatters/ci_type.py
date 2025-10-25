2960f9d0eb6341d8a08c4d4d0ddba12e58c420e5
David Harris <David_Harris@hmc.edu>
2025-10-24 19:37:07 -0700

    new Zca formatters

---
+++ generators/tests/testgen/src/testgen/instruction_formatters/ci_type.py
@@ -0,0 +1,26 @@
+# testgen/instruction_formatters/ci_type.py
+
+from testgen.data.instruction_params import InstructionParams
+from testgen.data.test_data import TestData
+from testgen.instruction_formatters.instruction_formatters import add_instruction_formatter
+from testgen.utils.common import load_int_reg, write_sigupd
+from testgen.utils.immediates import modify_imm
+
+
+@add_instruction_formatter("CI")
+def format_ci_type(
+    instr_name: str, test_data: TestData, params: InstructionParams
+) -> tuple[list[str], list[str], list[str]]:
+    """Format CI-type instruction."""
+    assert params.rs1 is not None and params.rs1val is not None
+    assert params.rd is not None
+    assert params.immval is not None
+    scaled_imm = modify_imm(params.immval, 6)
+    setup = [
+        load_int_reg("rs1", params.rs1, params.rs1val, test_data),
+    ]
+    test = [
+        f"{instr_name} x{params.rd}, {scaled_imm} # perform operation",
+    ]
+    check = [write_sigupd(params.rd, test_data, "int")]
+    return (setup, test, check)
